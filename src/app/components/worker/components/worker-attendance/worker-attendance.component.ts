f560be34f8b0d249faa310b2ff361926796554d3
Albin David C <albindavidc007@gmail.com>
2026-02-27 14:38:04 +0530

    feat: create clock in and clock out

---
+++ src/app/components/worker/components/worker-attendance/worker-attendance.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-worker-attendance',
+  imports: [],
+  templateUrl: './worker-attendance.component.html',
+  styleUrl: './worker-attendance.component.scss',
+})
+export class WorkerAttendanceComponent {
+
+}
